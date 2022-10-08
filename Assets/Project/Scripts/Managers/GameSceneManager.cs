b06764908535b727e5f0abf15f7d03691c04a2e7
Danny <danyl1607@gmail.com>
2022-10-08 10:36:40 +0300

    Update GameSceneManager.cs

--- Assets/Project/Scripts/Managers/GameSceneManager.cs
+++ Assets/Project/Scripts/Managers/GameSceneManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using PolyternityStuff.SceneManagement;
+using PolyternityStuff.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
